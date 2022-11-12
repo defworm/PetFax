dd7b0200ff9243353d76bbad29294bcf5be36f52
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-11-12 14:06:46 -0600

    reptile migration

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 # factory function
 
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:crystal@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
     @app.route("/")
-    def hello():
+    def index():
         return "Hello PetFax!!"
 
     from . import pet
